b2e6be2c678e92c5d69104ee4adce6271310254d
Muhammad Manzar Abbas <41924622+MManzar786@users.noreply.github.com>
2023-03-17 08:16:21 +0500

    auth added

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import * as cartActions from '../../../state/cart/cart.action';
 import * as cartSelector from '../../../state/cart/cart.selector';
 import { Observable } from 'rxjs';
 import { ProductI } from 'src/app/products/models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -20,15 +21,16 @@ export class NavbarComponent implements OnInit {
   role$ = this.store.select(fromAuthSelector.selectRole);
   cartItemsCount$!: Observable<any>;
   cartItems$!: Observable<ProductI>;
-  constructor(private store: Store<fromAuthReducer.State>) {}
+  constructor(
+    private store: Store<fromAuthReducer.State>,
+    private router: Router
+  ) {}
   ngOnInit(): void {
-    // this.store
-    //   .select(cartSelector.selectCartItems)
-    //   .subscribe((items) => (this.cartItemsCount = 0));
     this.cartItems$ = this.store.select(cartSelector.selectCartItems);
     this.cartItemsCount$ = this.store.select(cartSelector.selectCartItemsCount);
   }
   onLogoutClick() {
-    this.store.dispatch(authActions.logout());
+    localStorage.clear();
+    this.router.navigate(['/auth/sign-in']);
   }
 }
